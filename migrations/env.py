62aea9e7ce8e769d9a14d1fa767552c5d0c37e53
tracerwhite <tracerwhite@gmail.com>
2024-12-08 18:09:20 +0300

    Версия с миграциями alembic

---
+++ migrations/env.py
@@ -0,0 +1,41 @@
+from app.backend.db import Base
+from app.models import User, Task
+from sqlalchemy import engine_from_config, pool
+from alembic import context
+
+config = context.config
+
+config.set_main_option('sqlalchemy.url', 'sqlite:///taskmanager.db')
+
+target_metadata = Base.metadata
+
+def run_migrations_offline():
+    """Run migrations in 'offline' mode."""
+    url = config.get_main_option("sqlalchemy.url")
+    context.configure(url=url, target_metadata=target_metadata)
+
+    with context.begin_transaction():
+        context.run_migrations()
+
+def run_migrations_online():
+    """Run migrations in 'online' mode."""
+    connectable = engine_from_config(
+        config.get_section(config.config_ini_section),
+        prefix='sqlalchemy.',
+        poolclass=pool.NullPool,
+    )
+
+    with connectable.connect() as connection:
+        context.configure(
+            connection=connection,
+            target_metadata=target_metadata,
+            process_bind_param=True,
+        )
+
+        with context.begin_transaction():
+            context.run_migrations()
+
+if context.is_offline_mode():
+    run_migrations_offline()
+else:
+    run_migrations_online()
